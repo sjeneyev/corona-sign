f4de27a45e83bfc297a28a9558fa2585b919d40e
sjeneyev <sjeneyev@gmail.com>
2021-01-07 16:55:00 +0200

    initial commit

---
+++ backend/app.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const cors = require("cors");
+require("./db/mongoose");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+module.exports = app;
---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoronaSignComponent } from './corona-sign/corona-sign.component';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoronaSignComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatInputModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
