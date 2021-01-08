14604f3d904b7aa1cbd4a06042e66b2be2997f73
sjeneyev <sjeneyev@gmail.com>
2021-01-08 10:01:41 +0200

    commit

--- backend/src/models/signature.js
+++ backend/src/models/signature.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
-const signatureSchema = mongoose.Schema({})
+const signatureSchema = mongoose.Schema({
+    // TODO: create the model
+})
 
 
 const Signature = mongoose.model("Signature", signatureSchema);
--- backend/src/routers/signature.js
+++ backend/src/routers/signature.js
@@ -2,6 +2,9 @@ const express = require("express");
 const upload = require("../middleware/upload");
 const router = new express.Router();
 
+// TODO: Post route to add signature
+// TODO: Get Route to retrieve daily and send email
+// TODO: Get Route to retrieve daily/weekly/monthly
 
 
 module.exports = router;
